e76141668957d398f2349105fbbc95f8c4cd9af8
lungile_Sigasa <43475787+lungileS@users.noreply.github.com>
2019-09-24 02:59:14 +0200

    Update Deck.java
    Deck skeleton code. the methods might need a bit more consideration especially thinking about how the deck class is going to be used

--- Deck.java
+++ Deck.java
@@ -1,3 +1,32 @@
 public class Deck{
 
+   private Card[] deck;
+   
+   //constructors
+   public Deck(){
+      this.deck = new Card[52];
+      
+   }
+ 
+   //methods
+   public Card[] newDeck(){
+      //create a new deck of 52 cards
+   } 
+   
+   public void Shuffle(){
+      //shuffle this deck
+   }
+   
+   public String ShannonEntropy(){
+      //return the Shannon entropy of the deck
+    
+   }
+   
+   public String toString(){
+      //return a string form of the deck, i.e. a string telling what is contained in the deck
+   }
+   
+   public boolean equals(){
+      //checks if two decks are equal
+   }
 }
